b4e86a816ba2c167be6d170ff9381aaca99f2f7d
Eliezer Steinbock <eliesteinbock@gmail.com>
2021-04-03 21:29:18 +0300

    Use env vars

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -1,23 +1,43 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Post } from './posts/entities/post.entity';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
-import { Comment } from './comments/entities/comment.entity';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'mysecretpassword',
-      database: 'admin_demo',
-      entities: [Post, Comment],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<PostgresConnectionOptions> => {
+        const config: PostgresConnectionOptions = {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST', 'localhost'),
+          port: configService.get<number>('DB_PORT', 5432),
+          username: configService.get<string>('DB_USERNAME', 'postgres'),
+          password: configService.get<string>(
+            'DB_PASSWORD',
+            'mysecretpassword',
+          ),
+          database: configService.get<string>('DB_DATABASE_NAME', 'admin_demo'),
+          entities: ['dist/**/*.entity.js'],
+          // this should be false in production
+          synchronize: configService.get<boolean>('DB_SYNC', true),
+          logging: configService.get<boolean>('TYPEORM_LOGGING', false),
+          migrationsRun: false,
+          migrations: ['dist/migration/*.js'],
+          cli: {
+            migrationsDir: 'migration',
+          },
+        };
+
+        return config;
+      },
+      inject: [ConfigService],
     }),
     PostsModule,
     CommentsModule,
